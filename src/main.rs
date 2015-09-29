8d1d17782cbb4a9844c189dc907954ba74b1c37c
markrendle <mark@markrendle.net>
2015-09-29 19:37:42 +0100

    Use JSON, TOML parser does weird stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 extern crate glob;
-extern crate toml;
-use toml::Parser;
+extern crate rustc_serialize;
+extern crate rustache;
+
 use std::error::Error;
 use std::fs::File;
-use std::path::PathBuf;
+use std::path::Path;
 use std::io::prelude::*;
 use glob::glob;
+use rustc_serialize::json::Json;
 
-fn read(path: PathBuf) {
+fn load(path: &Path) -> String {
     let display = path.display();
     let mut file = match File::open(&path) {
         Err(why) => panic!("couldn't open {}: {}", display,
@@ -20,14 +22,23 @@ fn read(path: PathBuf) {
         Err(why) => panic!("couldn't read {}: {}", display, Error::description(&why)),
         Ok(_) => ()
     }
-    let value = Parser::new(&s).parse().unwrap();
-    println!("{} content:\n{:?}", display, value);
+    return s;
+}
+
+fn read(path: &Path, tmpl: &String) {
+    let s = load(path);
+    let data = Json::from_str(&s).unwrap();
+    let rv = rustache::render_text(tmpl, data).unwrap().unwrap();
+    let output = String::from_utf8(rv).unwrap();
+    println!("{}", output);
 }
 
 fn main() {
-    for entry in glob("**/*.toml").unwrap() {
+    let tmpl = load(Path::new("./sandbox/simple.tmpl"));
+
+    for entry in glob("./sandbox/**/*.json").unwrap() {
         match entry {
-            Ok(path) => read(path),
+            Ok(path) => read(&path, &tmpl),
             Err(e) => println!("{:?}", e)
         }
     }
